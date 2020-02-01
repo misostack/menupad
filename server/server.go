39cd8aecb95d5be8f7520a325cdf799a3b01718e
misostack <misostack.com@gmail.com>
2020-02-01 12:02:07 +0700

    Server with html pages

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 	"strconv"
 	"log"
 )
@@ -13,10 +14,25 @@ type Config struct {
 	Port uint16
 }
 
-type HelloHandler struct {}
+type HelloHandler struct {
+	Title string
+}
 
 func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello"))
+	// w.Write([]byte(h.output))
+	check := func(err error) {
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	t, err := template.New("webpage").Parse(MainLayout())
+	check(err)
+	data := struct {
+		Title string
+	}{
+		Title: h.Title,
+	}
+	err = t.Execute(w, data)
 }
 
 // Init : init the webserver
@@ -26,9 +42,26 @@ func Init(cfg Config) {
 		Addr: addr,
 		Handler: nil,
 	}
-	hello := HelloHandler{}
+	hello := HelloHandler{ Title: "hello" }
+	world := HelloHandler{ Title: "world" }
 	// Attach handles to DefaultServerMux
 	http.Handle("/hello", &hello)
+	http.Handle("/world", &world)
 	fmt.Printf("Menupad API version %v is running at %v\n", API_VERSION, addr)
 	log.Fatal(multiplexer.ListenAndServe())
+}
+
+func MainLayout() string{
+	const tpl = `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>{{.Title}}</title>
+		</head>
+		<body>
+			<h1>{{.Title}}</h1>
+		</body>
+	</html>`
+	return tpl
 }
