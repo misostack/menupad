e6801331d2f1e250c238c8cffb82e62fcb6ad813
misostack <misostack.com@gmail.com>
2020-02-01 11:12:38 +0700

    Add hello handle

--- server/server.go
+++ server/server.go
@@ -11,7 +11,13 @@ const API_VERSION = 0.1
 
 type Config struct {
 	Port uint16
-} 
+}
+
+type HelloHandler struct {}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello"))
+}
 
 // Init : init the webserver
 func Init(cfg Config) {	
@@ -20,6 +26,9 @@ func Init(cfg Config) {
 		Addr: addr,
 		Handler: nil,
 	}
+	hello := HelloHandler{}
+	// Attach handles to DefaultServerMux
+	http.Handle("/hello", &hello)
 	fmt.Printf("Menupad API version %v is running at %v\n", API_VERSION, addr)
 	log.Fatal(multiplexer.ListenAndServe())
 }
